237b1228393d308ce9fcd0e47d89686049b2bbe0
Ewa Manek <manekej@gmail.com>
2016-12-15 09:55:54 -0800

    Add calendar colors

--- app/src/main/java/com/example/guest/habittracker/ui/CalendarActivity.java
+++ app/src/main/java/com/example/guest/habittracker/ui/CalendarActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.guest.habittracker.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.roomorama.caldroid.CaldroidFragment;
@@ -12,22 +15,55 @@ import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity {
 
+    private CaldroidFragment mCaldroidFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        mCaldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
         Calendar cal = Calendar.getInstance();
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
         args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
-        caldroidFragment.setArguments(args);
+        args.putInt(CaldroidFragment.THEME_RESOURCE, R.style.CaldroidCustom);
+        mCaldroidFragment.setArguments(args);
 
         android.support.v4.app.FragmentTransaction t = getSupportFragmentManager().beginTransaction();
-        t.replace(R.id.cal, caldroidFragment);
+        t.replace(R.id.cal, mCaldroidFragment);
         t.commit();
 
+        populateDates();
+    }
+
+    private void populateDates() {
         DatabaseReference userDates = FirebaseDatabase.getInstance().getReference("users").child("dates");
+        userDates.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
