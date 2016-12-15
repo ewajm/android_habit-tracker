3a139ec213792850c9c759dcae42e0d35eb8d15b
Ewa Manek <manekej@gmail.com>
2016-12-15 13:41:58 -0800

    Add listview to calendar detail fragment which displays recorded activities for that day or a message if there are no activities

--- app/src/main/java/com/example/guest/habittracker/ui/CalendarDetailFragment.java
+++ app/src/main/java/com/example/guest/habittracker/ui/CalendarDetailFragment.java
@@ -12,6 +12,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.guest.habittracker.R;
+import com.example.guest.habittracker.models.Activity;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,6 +28,8 @@ public class CalendarDetailFragment extends Fragment {
     @Bind(R.id.dateHeadingView) TextView mDateHeadingView;
     @Bind(R.id.activityListView) ListView mActivityListView;
     String mDate;
+    String mUserId;
+    FirebaseListAdapter<Activity> mAdapter;
 
 
     public CalendarDetailFragment() {
@@ -33,6 +40,7 @@ public class CalendarDetailFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mDate = getArguments().getString("date");
+        mUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
     @Override
@@ -42,7 +50,30 @@ public class CalendarDetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_calendar_detail, container, false);
         ButterKnife.bind(this, view);
         mDateHeadingView.setText(mDate);
+        makeActivityListView();
+        mActivityListView.setEmptyView(view.findViewById(android.R.id.empty));
+        mActivityListView.setAdapter(mAdapter);
         return view;
+    }
+
+    private void makeActivityListView() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users").child(mUserId).child("dates").child(mDate);
+        mAdapter = new FirebaseListAdapter<Activity>(getActivity(), Activity.class, android.R.layout.two_line_list_item, ref) {
+            @Override
+            protected void populateView(View v, Activity model, int position) {
+                String[] motivations = {"meh", "fine", "do things", "PARKOUR"};
+                ((TextView)v.findViewById(android.R.id.text1)).setText(model.getName());
+                ((TextView)v.findViewById(android.R.id.text2)).setText(motivations[model.getMotivationLevel()-1]);
+            }
+        };
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mAdapter != null){
+            mAdapter.cleanup();
+        }
     }
 
 }
